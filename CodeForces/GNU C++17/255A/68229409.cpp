a448b182c767fa49d1ffa379f276f193de74bfc6
Akshay S <Akshayshinde682@gmail.com>
2022-06-02 22:38:43 +0530

    Committing files

---
+++ CodeForces/GNU C++17/255A/68229409.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#define all(v) v.begin(),v.end()
+#define rep(a,n,b) for(ll  i=a;i<n;i+=b)
+#define rrep(a,n,b) for(ll i=n;i>=a;i-=b)
+#define iter(obj) for(auto x : obj) 
+#define pb(i) push_back(i)
+#define ff first
+#define ss second
+#define pchr(flag) cout<<(flag ? "YES" : "NO")
+#define ll long long int
+#define input freopen("input.txt","r",stdin)
+#define op freopen("output.txt","w",stdout);
+#define fastio std::ios::sync_with_stdio(false)
+using namespace std;
+const int N = 2e5+7;
+const int MOD  = 1000000007;
+const float PI = 3.1415926535;
+//33333333333;
+int prime[N];
+void SieveOfEratosthenes(int n) 
+{ 
+     for (int p=2; p*p<=n; p++) {
+        if (prime[p] == 0)  {
+            for (int i=p*p; i<=n; i += p) 
+                prime[i] = 1; 
+        } 
+    } 
+} 
+int main() 
+{ 
+	// input;
+	fastio;
+	int n,temp,mx=0,index=0;
+	cin>>n;
+	int arr[3];
+	string a[3] = {"chest","biceps","back"};
+	fill(arr,arr+3,0);
+	rep(0,n,1) {
+		cin>>temp;
+		arr[i%3]+=temp;
+	}
+	rep(0,3,1) {
+		if(arr[i] > mx) {
+			mx = arr[i];
+			index = i;
+		}
+	}
+	cout<<a[index];
+}
+
+
+ 

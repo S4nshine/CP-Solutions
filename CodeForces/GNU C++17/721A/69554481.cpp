1142af7379bf2cf83db8dd1ab5907dfd57b2a182
Akshay S <Akshayshinde682@gmail.com>
2022-06-02 22:34:06 +0530

    Committing files

---
+++ CodeForces/GNU C++17/721A/69554481.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#define all(v) v.begin(),v.end()
+#define rep(a,n,b) for(int i=a;i<n;i+=b)
+#define rrep(a,n,b) for(ll i=n;i>=a;i-=b)
+#define iter(obj) for(auto x : obj) 
+#define pb(i) push_back(i)
+#define ff first
+#define ss second
+#define pchr(flag) cout<<(flag ? "YES" : "NO")
+#define ot(x) cout<<x
+#define nl cout<<endl;
+#define sc cout<<" "
+#define ll long long int
+#define input freopen("input.txt","r",stdin)
+#define op freopen("output.txt","w",stdout);
+#define fastio std::ios::sync_with_stdio(false)
+using namespace std;
+const int N = 2e5+7;
+const int MOD  = 1000000007;
+const float PI = 3.1415926535;
+//33333333333;
+int prime[N];
+void SieveOfEratosthenes(int n) 
+{ 
+     for (int p=2; p*p<=n; p++) {
+        if (prime[p] == 0)  {
+            for (int i=p*p; i<=n; i += p) 
+                prime[i] = 1; 
+        } 
+    } 
+} 
+int main() 
+{ 
+	fastio;
+	int n,c=0;
+	cin>>n;
+	string a;
+	cin>>a;
+	queue<int>s;
+	rep(0,n,1) {
+		if(a[i] == 'B') {
+			while(a[i] == 'B' and i <n)  {
+				i++;
+				c++;
+			}
+			s.push(c);
+			c = 0;
+		}
+
+	}
+	cout<<s.size()<<endl;
+	while(!s.empty()) {
+		cout<<s.front()<<" ";
+		s.pop();
+	}
+}

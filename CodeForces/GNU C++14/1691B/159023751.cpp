bed72d9f156b3694a919ba948c6dcceb00acba69
Akshay S <Akshayshinde682@gmail.com>
2022-06-02 22:55:25 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1691B/159023751.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#define ff first
+#define ss second
+#define ll long long int
+#define loop(n) for(int i=0;i<n;i++)
+#define pflag(flag) if(flag) cout<<"YES"; else cout<<"NO";
+#define input freopen("input.txt","r",stdin)
+#define op freopen("output.txt","w",stdout);
+#define fastio std::ios::sync_with_stdio(false)
+using namespace std;
+//333333333333
+
+
+
+bool isvowel(char s) {
+  if(s == 'a' or s == 'e' or s == 'i' or s =='o' or s == 'u') return true;
+  return false;
+}
+int main() {
+  // input;
+  int t;
+  cin>>t;
+  while(t--) {
+    int n,ans=1;
+    cin>>n;
+    int arr[n];
+    int dup[n];
+    for(int i=0;i<n;i++) dup[i] = i+1;
+    map<int,int>mp;
+    loop(n){
+      cin>>arr[i];
+      mp[arr[i]]+=1;
+    }
+    for(auto x : mp) {
+      if(x.ss == 1) {
+        ans = 0;
+        break;
+      }
+    }
+    if(ans == 0 ) cout<<-1<<endl;
+    else  {
+      for(int i=0;i<n-1;i++) {
+        if(arr[i] == arr[i+1]) 
+          swap(dup[i],dup[i+1]);
+      }
+      for(int i=0;i<n;i++) cout<<dup[i]<<" ";
+    }
+    cout<<endl;
+  }
+}

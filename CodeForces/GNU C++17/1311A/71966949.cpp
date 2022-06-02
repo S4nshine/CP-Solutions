2cfa984dabccba87786738075089458e33a93b88
Akshay S <Akshayshinde682@gmail.com>
2022-06-02 22:51:26 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1311A/71966949.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#define all(v) v.begin(),v.end()
+#define rep(a,n,b) for(ll i=a;i<n;i+=b)
+#define rrep(a,n,b) for(ll i=n;i>=a;i-=b)
+#define iter(obj) for(auto x : obj) 
+#define pb(i) push_back(i)
+#define ff first
+#define ss second
+#define pchr(flag) cout<<(flag ? "YES" : "NO")
+#define ot(x) cout<<x
+#define nl cout<<endl;
+#define sc cout<<" "
+#define ll long long int
+#define input freopen("input.txt","r",stdin)
+#define op freopen("output.txt","w",stdout);
+#define fastio std::ios::sync_with_stdio(false)
+using namespace std;
+const int N = 2e5+7;
+const int MOD  = 1000000007;
+const float PI = 3.1415926535;
+//33333333333;
+int prime[N];
+void SieveOfEratosthenes(int n) 
+{ 
+     for (int p=2; p*p<=n; p++) {
+        if (prime[p] == 0)  {
+            for (int i=p*p; i<=n; i += p) 
+                prime[i] = 1; 
+        } 
+    } 
+} 
+int main() {
+	int t;
+	cin>>t;
+	while(t--) {
+		int a,b;
+        cin>>a>>b;
+        if(a == b) cout<<0<<endl;
+        else  {
+            int x = abs(a-b);
+            if(a > b) {
+                if(x%2==0) cout<<1<<endl;
+                else cout<<2<<endl;
+            }
+            else if(a < b) {
+                if(x%2) cout<<1<<endl;
+                else cout<<2<<endl;
+            }
+        }
+	}
+	return 0;	
+}

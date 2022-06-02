69a5e254ca447f9ed9f43b5245d9f3156e355563
Akshay S <Akshayshinde682@gmail.com>
2022-06-02 22:30:20 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1660A/155834219.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#define all(v) v.begin(),v.end()
+#define rep(a,n,b) for(ll i=a;i<n;i+=b)
+#define rrep(a,n,b) for(ll i=n;i>=a;i-=b)
+#define iter(obj) for(auto x : obj) 
+#define pb(i) push_back(i)
+#define ff first
+#define ss second
+#define pchr(flag) cout<<(flag ? "YES" : "NO")
+#define ll long long int
+#define input freopen("input.txt","r",stdin)
+#define op freopen("output.txt","w",stdout);
+#define fastio std::ios::sync_with_stdio(false)
+using namespace std;
+const int N = 2e5+7;
+const int MOD  = 1000000007;
+const float PI = 3.1415926535;
+//33333333333;
+int prime[N];
+void SieveOfEratosthenes(int n) 
+{ 
+     for (int p=2; p*p<=n; p++) {
+        if (prime[p] == 0)  {
+            for (int i=p*p; i<=n; i += p) 
+                prime[i] = 1; 
+        } 
+    } 
+} 
+bool isvowel(char s ) {
+    if(s == 'a' or s == 'e' or s == 'i' or s == 'o' or s == 'u') return true;
+    return false;
+}
+void DFSUtil(int u, vector<int> adj[],
+                    vector<bool> &visited)
+{
+    visited[u] = true;
+    cout << u << " ";
+    for (int i=0; i<adj[u].size(); i++)
+        if (visited[adj[u][i]] == false)
+            DFSUtil(adj[u][i], adj, visited);
+}
+void DFS(vector<int> adj[], int V)
+{
+    vector<bool> visited(V, false);
+    for (int u=0; u<V; u++)
+        if (visited[u] == false)
+            DFSUtil(u, adj, visited);
+}
+int main() {
+   // input;
+  int t;
+  cin>>t;
+  while(t--) {
+    int  a,b;
+    cin>>a>>b;
+    if(a==0) cout<<1<<endl;
+    else cout<<2*b+a+1<<endl;
+   }
+} 

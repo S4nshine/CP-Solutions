6c5b6446f1594d706792186ef712bec8dcf0fab1
Akshay S <Akshayshinde682@gmail.com>
2022-06-02 22:39:10 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1257A/64809260.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+#define all(v) v.begin(),v.end()
+#define rep(a,n,b) for(int i=a;i<n;i+=b)
+#define rrep(a,n,b) for(int i=n;i>=a;i-=b)
+#define iter(obj) for(auto x : obj) 
+#define pb(i) push_back(i)
+#define ff first
+#define ss second
+#define pchr(flag) cout<<(flag ? "YES" : "NO")
+#define ll long long int
+#define input freopen("input.txt","r",stdin)
+#define op freopen("output.txt","w",stdout);
+#define fastio std::ios::sync_with_stdio(false)
+using namespace std;
+const int N = 2e5+7;
+const int MOD  = 1000000007;
+const float PI = 3.1415926535;
+//33333333333;
+int arr[N];
+int main()  {
+	// input;
+	fastio;
+	int t;
+	cin>>t;
+	while (t--)
+	{
+		int  n,x,a,b;
+		cin>>n>>x>>a>>b;
+		int ans = n - 1;
+		int c = min(a,b);
+		int v = max(a,b);
+		if(v-c>= ans) {
+			cout<<v-c<<endl;
+			continue;
+		}
+		while(c > 1 and  x)  {
+			c--;
+			x--;
+		}
+		if(x) {
+			while(v  < n and x ) {
+				v++;
+				x--;
+			}
+		}
+		cout<<v-c<<endl;
+	}
+	return 0;
+}

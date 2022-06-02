5984f70cd3d903c7acd10beb75f0bf53cc06aa65
Akshay S <Akshayshinde682@gmail.com>
2022-06-02 22:29:59 +0530

    Committing files

---
+++ CodeChef/C++17/FOOTCUP/64299690.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define all(v) v.begin(),v.end()
+#define rep(a,n,b) for(ll i=a;i<n;i+=b)
+#define rrep(a,n,b) for(ll i=n;i>=a;i-=b)
+#define iter(obj) for(auto x : obj) 
+#define pb(i) push_back(i)
+#define ff first
+#define ss second
+#define pchr(flag) cout<<(flag ? "YES" : "NO")
+#define ll long long int
+#define input freopen("input.txt","r",stdin)
+#define op freopen("output.txt","w",stdout);
+#define fastio std::ios::sync_with_stdio(false)
+using namespace std;
+const int N = 2e5+7;
+const int MOD  = 1000000007;
+const float PI = 3.1415926535;
+//33333333333;
+int prime[N];
+void SieveOfEratosthenes(int n) 
+{ 
+     for (int p=2; p*p<=n; p++) {
+        if (prime[p] == 0)  {
+            for (int i=p*p; i<=n; i += p) 
+                prime[i] = 1; 
+        } 
+    } 
+} 
+bool isvowel(char s ) {
+    if(s == 'a' or s == 'e' or s == 'i' or s == 'o' or s == 'u') return true;
+    return false;
+}
+int main() {
+   // input;
+   int t;
+   cin>>t;
+   while(t--) {
+    int x,y;
+    cin>>x>>y;
+    if(x+y >=1 and x == y) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl; 
+   }
+} 

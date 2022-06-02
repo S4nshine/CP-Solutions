f00c925a307df7c7241523fb95e0a10521ccb71a
Akshay S <Akshayshinde682@gmail.com>
2022-06-02 22:30:09 +0530

    Committing files

---
+++ CodeChef/C++17/WATERCOOLER2/62441896.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define all(v) v.begin(),v.end()
+#define rep(a,n,b) for(ll i=a;i<n;i+=b)
+#define rrep(a,n,b) for(ll i=n;i>=a;i-=b)
+#define iter(obj) for(auto x : obj) 
+#define pb(i) push_back(i)
+#define ff first
+#define ss second
+#define pchr(flag) cout<<(flag ? "YES" : "NO")
+#define ll long long int
+#define input freopen("input.txt","r",stdin)
+#define op freopen("output.txt","w",stdout);
+#define fastio std::ios::sync_with_stdio(false)
+using namespace std;
+const int N = 2e5+7;
+const int MOD  = 1000000007;
+const float PI = 3.1415926535;
+//33333333333;
+
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--) {
+       int x,y;
+       cin>>x>>y;
+       if(y % x) cout<<y/x;
+       else cout<<y/x-1<<endl;
+       cout<<endl;
+    }
+}

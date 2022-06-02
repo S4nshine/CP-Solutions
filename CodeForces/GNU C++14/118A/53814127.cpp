283bce32e907a8719504bef10f897d7981a46bd1
Akshay S <Akshayshinde682@gmail.com>
2022-06-02 22:40:12 +0530

    Committing files

---
+++ CodeForces/GNU C++14/118A/53814127.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define all(v) v.begin(),v.end()
+#define rep(a,n,b) for(long int i=a;i<n;i+=b)
+#define pb(i) push_back(i)
+#define ll long long int
+#define PI 3.1415926535
+#define INTMAX 100000000000000000
+#define mod 10000000007
+#define input freopen("input.txt","r",stdin)
+#define fastio std::ios::sync_with_stdio(false)
+using namespace std;
+int main() {
+  // input;     
+  fastio;
+  string vowels = "AEIOUYaeiouy";
+  string a;
+  cin>>a;
+  rep(0,a.size(),1) {
+    if(vowels.find(a[i]) != string::npos)  ;
+    else cout<<"."<<char(tolower(a[i]));
+  }
+}

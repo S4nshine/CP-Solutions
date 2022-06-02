76447ec7c2cab6c7d49e6f887cfb7f5b114a9492
Akshay S <Akshayshinde682@gmail.com>
2022-06-02 22:39:28 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1196A/61266818.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define all(v) v.begin(),v.end()
+#define rep(a,n,b) for(int i=a;i<n;i+=b)
+#define rrep(a,n,b) for(int i=n;i>=a;i-=b)
+#define iter(obj) for(auto x : obj) 
+#define pb(i) push_back(i)
+#define ff first
+#define ss second
+#define pchr(flag) cout<<(flag ? "YES" : "NO")
+#define PI 3.1415926535
+#define ll long long int
+#define mod 1000000007
+#define input freopen("input.txt","r",stdin)
+#define fastio std::ios::sync_with_stdio(false)
+using namespace std;
+//333333333;
+int main()  {
+	//input;
+	fastio;
+	int t;
+	cin>>t;
+	while(t--) {
+		unsigned ll a,b,c;
+		cin>>a>>b>>c;
+		cout<<(a+b+c)/2<<endl;
+	}
+}

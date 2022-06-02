cda668ee0419cd2b76a5a5ac47922f98416ce4c6
Akshay S <Akshayshinde682@gmail.com>
2022-06-02 22:29:32 +0530

    Committing files

---
+++ CodeForces/GNU C++14/379A/158153167.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define ff first
+#define ss second
+#define ll long long int
+#define loop(n) for(int i=0;i<n;i++)
+#define pflag(flag) if(flag) cout<<"YES"; else cout<<"NO";
+#define input freopen("input.txt","r",stdin)
+#define op freopen("output.txt","w",stdout);
+#define fastio std::ios::sync_with_stdio(false)
+using namespace std;
+//333333333333
+
+
+
+bool isvowel(char s) {
+  if(s == 'a' or s == 'e' or s == 'i' or s =='o' or s == 'u') return true;
+  return false;
+}
+int main() {
+  // input;
+  int a,b;
+  cin>>a>>b;
+  int ans = 0,res=0;
+  while(1) {
+    ans+=a;
+    res+=a;
+    if(res < b) break;
+    a = res /b;
+    res %=b;
+  }
+  cout<<ans;
+}

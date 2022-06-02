04512924750be690a2f2e8672de6686feb6f7023
Akshay S <Akshayshinde682@gmail.com>
2022-06-02 22:40:15 +0530

    Committing files

---
+++ CodeForces/GNU C++14/276A/53812939.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define all(v) v.begin(),v.end()
+#define rep(a,n,b) for(long int i=a;i<n;i+=b)
+#define pb(i) push_back(i)
+#define ll long long int
+#define PI 3.1415926535
+#define INTMAX 100000000000000000
+#define mod 10000000007
+#define input freopen("input.txt","r",stdin)
+#define fastio std::ios::sync_with_stdio(false)
+using namespace std;
+int main() {
+  // input;
+  fastio;
+  int n,k,ans=INT_MIN;
+  cin>>n>>k;
+  rep(0,n,1) {
+   int a,b;
+    cin>>a>>b;
+    if(b > k) ans = max(ans,a-(b-k));
+    else   ans = max(ans,a);
+  } cout<<ans;
+}

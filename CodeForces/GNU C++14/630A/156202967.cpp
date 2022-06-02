1e42cf9b35fe8cc88f83c7103102b4b2f2ca74fd
Akshay S <Akshayshinde682@gmail.com>
2022-06-02 22:30:01 +0530

    Committing files

---
+++ CodeForces/GNU C++14/630A/156202967.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ff first
+#define ss second
+#define ll long long int
+#define pflag(flag) if(flag) cout<<"YES"; else cout<<"NO";
+#define input freopen("input.txt","r",stdin)
+#define op freopen("output.txt","w",stdout);
+#define fastio std::ios::sync_with_stdio(false)
+using namespace std;
+//333333333333
+
+
+int main() {
+  input;
+  int n;
+  cin>>n;
+  cout<<25;
+} 

ef0fccf5303c94a9bc7c01865024aeb9d5c0ae6e
Akshay S <Akshayshinde682@gmail.com>
2022-06-02 22:38:41 +0530

    Committing files

---
+++ CodeForces/GNU C++17/237A/68230251.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#define all(v) v.begin(),v.end()
+#define rep(a,n,b) for(ll  i=a;i<n;i+=b)
+#define rrep(a,n,b) for(ll i=n;i>=a;i-=b)
+#define iter(obj) for(auto x : obj) 
+#define pb(i) push_back(i)
+#define ff first
+#define ss second
+#define pchr(flag) cout<<(flag ? "YES" : "NO")
+#define ll long long int
+#define input freopen("input.txt","r",stdin)
+#define op freopen("output.txt","w",stdout);
+#define fastio std::ios::sync_with_stdio(false)
+using namespace std;
+const int N = 2e5+7;
+const int MOD  = 1000000007;
+const float PI = 3.1415926535;
+//33333333333;
+int prime[N];
+void SieveOfEratosthenes(int n) 
+{ 
+     for (int p=2; p*p<=n; p++) {
+        if (prime[p] == 0)  {
+            for (int i=p*p; i<=n; i += p) 
+                prime[i] = 1; 
+        } 
+    } 
+} 
+int main() 
+{ 
+	// input;
+	fastio;
+	int t,ans=1;
+	cin>>t;
+	map< pair<int,int>,int>mp;
+	while(t--) {
+		int x,y;
+		cin>>x>>y;
+		mp[{x,y}]+=1;
+	}
+	for(auto x : mp) {
+		ans  = max(x.ss,ans);
+	}
+	cout<<ans;
+}
+
+
+ 

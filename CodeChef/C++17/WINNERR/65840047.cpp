0f4c4e066ee2ab5ed24775f9ffe2ee33b8337ac5
Akshay S <Akshayshinde682@gmail.com>
2022-06-02 22:29:35 +0530

    Committing files

---
+++ CodeChef/C++17/WINNERR/65840047.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define ff first
+#define ss second
+#define ll long long int
+#define loop(n) for(int i=0;i<n;i++)
+#define pflag(flag) if(flag) cout<<"YES"; else cout<<"NO";
+#define input freopen("input.txt","r",stdin)
+#define op freopen("output.txt","w",stdout);
+#define fastio std::ios::sync_with_stdio(false)
+using namespace std;
+//333333333333
+
+
+
+bool isvowel(char s) {
+  if(s == 'a' or s == 'e' or s == 'i' or s =='o' or s == 'u') return true;
+  return false;
+}
+int main() {
+  // input;
+  int t;
+  cin>>t;
+  while(t--) {
+    int x,y,a,b;
+    cin>>x>>y>>a>>b;
+    int ans = (max(x,y)-max(a,b));
+    if(ans == 0) cout<<"TIE"<<endl;
+    else if(ans < 0 ) cout<<"P"<<endl;
+    else cout<<"Q"<<endl;
+  }
+}

c87154452b69b0ccf5bd22b51bc6fef65514ca95
Akshay S <Akshayshinde682@gmail.com>
2022-06-02 22:39:01 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1272C/66711471.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#
+#define all(v) v.begin(),v.end()
+#define rep(a,n,b) for(int i=a;i<n;i+=b)
+#define rrep(a,n,b) for(int i=n;i>=a;i-=b)
+#define iter(obj) for(auto x : obj) 
+#define pb(i) push_back(i)
+#define ff first
+#define ss second
+#define pchr(flag) cout<<(flag ? "YES" : "NO")
+#define ll long long int
+#define input freopen("input.txt","r",stdin)
+#define op freopen("output.txt","w",stdout);
+#define fastio std::ios::sync_with_stdio(false)
+using namespace std;
+const int N = 2e5+7;
+const int MOD  = 1000000007;
+const float PI = 3.1415926535;
+ll arr[N];
+//33333333333;
+int main() 
+{ 
+	// input;
+	fastio;
+	unsigned long long int n,k,ans=0,c=0;
+	cin>>n>>k;
+	string a;
+	cin>>a;
+	char x;
+	while(k--) {
+		cin>>x;
+		arr[x-'a'] = 1;
+	}
+	rep(0,n,1) {
+		if(arr[a[i]-'a']) {
+			while(arr[a[i]-'a'] and i < n) {
+				i++;
+				c++;
+			}
+			ans+=(c*(c+1))/2;
+			c =0;
+		}
+	} 
+	cout<<ans<<endl;
+} 
